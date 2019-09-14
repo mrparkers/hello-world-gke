aa1f358a4110386937fd82c5751dd3fed958bf0c
Michael Parker <michael@parker.gg>
2019-09-13 19:29:45 -0500

    Adds required google APIs

---
+++ tf/services.tf
@@ -0,0 +1,15 @@
+/*
+  The APIs defined here are must be enabled in order to stand up this exercise.
+*/
+
+resource "google_project_service" "services" {
+  for_each = toset([
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+  ])
+
+  project = google_project.project.id
+  service = each.value
+}
